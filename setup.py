2e2e5fa8ef122a8d84360862297d4bfec42e6a86
Andy Kee <andykee@gmail.com>
2018-09-16 19:50:33 -0700

    fix package data

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ setup(
         author='Andy Kee',
         author_email='andykee@gmail.com',
         packages=['akurls'],
+        package_data={'akurls': ['parsers/*','theme/*']},
         install_requires=[
             'BeautifulSoup4 >= 4.4.1', 
             'feedparser >= 5.2.1',
             'Jinja2 >= 2.8',
             'MarkupSafe >= 0.23',
             'wheel >= 0.24.0',
-            'praw >= 3.6.1'],
+            'praw >= 3.6.1',
+            'boto3'],
         python_requires='>=3',
         )
